1316ad2d1eb664cb36fce9e88882ba87d72bc987
Mehrdad <mehrdad.arasteh@g.austincc.edu>
2026-07-05 09:51:58 -0500

    Fix setup.py syntax error

--- setup.py
+++ setup.py
@@ -1,1 +1,15 @@
-ECHO is on.
+from setuptools import setup, find_packages
+
+setup(
+    name="spring-mass-simulator",
+    version="0.1.0",
+    description="Interactive spring–mass–damper simulator with engineering dashboard",
+    author="Mehrdad",
+    packages=find_packages(),
+    install_requires=[
+        "numpy",
+        "matplotlib",
+        "scipy"
+    ],
+    python_requires=">=3.8",
+)
